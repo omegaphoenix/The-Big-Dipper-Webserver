fecd9c8e3f703a77f672e071fdd19d646d233a79
Justin Leong <justinleong16@gmail.com>
2015-04-15 15:23:37 -0700

    Updated .gitignore and added unit tests from class

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,61 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <sstream>
+#include <string>
 
+
+/*
+ * Original example test included in assignment.
+ */
 TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+    NginxConfigParser parser;
+    NginxConfig out_config;
 
-  bool success = parser.Parse("example_config", &out_config);
+    bool success = parser.Parse("example_config", &out_config);
 
-  EXPECT_TRUE(success);
+    EXPECT_TRUE(success);
+}
+
+
+/*
+ * Test for toString written in class.
+ */
+TEST(NginxConfigStatementTest, ToString)
+{
+    NginxConfigStatement statement;
+    statement.tokens_.push_back("foo");
+    statement.tokens_.push_back("bar");
+    EXPECT_EQ("foo bar;\n", statement.ToString(0));
+}
+
+class NginxStringConfigTest : public ::testing::Test
+{
+protected:
+    bool ParseString(const std::string &config_string)
+    {
+        std::stringstream config_stream(config_string);
+        return parser_.Parse(&config_stream, &config_);
+    }
+    NginxConfigParser parser_;
+    NginxConfig config_;
+};
+/*
+ * Test written in class.
+ */
+TEST_F(NginxStringConfigTest, SimpleTextConfig) {
+    EXPECT_TRUE(ParseString("foo bar;"));
+}
+/*
+ * Test written in class.
+ */
+TEST_F(NginxStringConfigTest, SimpleBadTextConfig) {
+    EXPECT_FALSE(ParseString("foo bar"));
+}
+TEST_F(NginxStringConfigTest, NestedConfig) {
+    EXPECT_TRUE(ParseString(
+            "foo { "
+            "   bar { "
+            "       baz aux;"
+            "   }"
+            "}"));
 }
