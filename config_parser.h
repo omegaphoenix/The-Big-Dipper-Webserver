1d136b6cbb23bbfe59193e17c8a48a3bbd8af026
Justin Leong <justinleong16@gmail.com>
2015-04-22 01:52:04 -0700

    Updated build_webserver.sh to compile config_parser.cc as well. Updated webserver_main.cc to read port using parser in config_parser. Modified config_parser.cc to stop printing tokens. Added parse function to set port to number specified in input config file in config_parser.

--- config_parser.h
+++ config_parser.h
@@ -4,6 +4,8 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <stdio.h>
+#include <string.h>
 
 class NginxConfig;
 
@@ -30,6 +32,7 @@ class NginxConfigParser {
   // Take a opened config file or file name (respectively) and store the
   // parsed config in the provided NginxConfig out-param.  Returns true
   // iff the input config file is valid.
+  bool Parse(const char* file_name, NginxConfig* config, int &portToSet);
   bool Parse(std::istream* config_file, NginxConfig* config);
   bool Parse(const char* file_name, NginxConfig* config);
 
