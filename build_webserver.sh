fc84c19a36c49f39ecb1e2c3846783963c6a1a04
Justin Leong <justinleong16@gmail.com>
2015-04-25 22:49:25 -0700

    Added getPort function based on HW 3 solutions. Replaced original implementation inside of config_parser.cc

--- build_webserver.sh
+++ build_webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 case `uname` in
-  Linux) g++ webserver.cc webserver_main.cc -std=c++0x -g -Wall -o webserver -lboost_system;;
-  Darwin) clang++ webserver.cc webserver_main.cc -std=c++11 -g -Wall -stdlib=libc++ -o config_parser -lboost_system;;
+  Linux) g++ webserver.cc webserver_main.cc config_parser.cc -std=c++0x -g -Wall -o webserver -lboost_system;;
+  Darwin) clang++ webserver.cc webserver_main.cc config_parser.cc -std=c++11 -g -Wall -stdlib=libc++ -o config_parser -lboost_system;;
   *) echo "Unknown operating system";;
 esac
--- webserver_main.cc
+++ webserver_main.cc
@@ -1,7 +1,58 @@
+// Based on Yichi's Hw 3 Solution
+
+#include <fstream>
+#include <iostream>
+#include <boost/asio.hpp>
 #include "webserver.h"
+#include "config_parser.h"
+
+
+int getPort(const NginxConfig &config)
+{
+    for(const auto& statement : config.statements_) 
+    {
+        bool k1 = true;
+        for(const std::string& token : statement->tokens_)
+        {
+            if (!k1)
+            {
+                try
+                {
+                    return stoi(token);
+                }
+                catch (...)
+                {
+                }
+            }
+            k1 = (token != "port");
+        }
+    }
+    return -1;
+}
+
 
 int main(int argc, char* argv[]) {
-    WebServer webserver;
-    webserver.helloWorld();
-    return 0;
+    if(argc == 2)
+    {
+        NginxConfigParser config_parser;
+        NginxConfig config;
+        if (!config_parser.Parse(argv[1], &config))
+        {
+            return -1;
+        }
+        int port = getPort(config);
+        //int port = 80;
+        if(port > 0)
+        {
+            //WebServer webserver;
+            WebServer webserver(port);
+            webserver.helloWorld();
+            return 0;
+        }
+    }
+    else
+    {
+        std::cerr << "Usage: ./webserver [config_file]" << std::endl;
+    }
+    return -1;
 }
