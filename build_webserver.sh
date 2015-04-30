2475ea046dc8771ba402950dcbd1514f7cf30f42
Justin Leong <justinleong16@gmail.com>
2015-04-30 00:04:56 -0700

    Removed build_*.sh files.

--- build_webserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-case `uname` in
-  Linux) g++ webserver.cc webserver_main.cc config_parser.cc -std=c++0x -g -Wall -o webserver -lboost_system;;
-  Darwin) clang++ webserver.cc webserver_main.cc config_parser.cc -std=c++11 -g -Wall -stdlib=libc++ -o webserver -lboost_system;;
-  *) echo "Unknown operating system";;
-esac
