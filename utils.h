e2555e9886bfff44863e3ec063a17411f2347620
Justin Leong <justinleong16@gmail.com>
2015-05-07 15:15:30 -0700

    Format for method to create mappings from config file

--- utils.h
+++ utils.h
@@ -1,11 +1,13 @@
+#include <map>
+#include <string>
 #include "config_parser.h"
 
 int getPort(const NginxConfig &config)
 {
-    for(const auto& statement : config.statements_) 
+    for (const auto& statement : config.statements_) 
     {
         bool k1 = true;
-        for(const std::string& token : statement->tokens_)
+        for (const std::string& token : statement->tokens_)
         {
             if (!k1)
             {
@@ -23,4 +25,56 @@ int getPort(const NginxConfig &config)
     return -1;
 }
 
-
+std::map<std::string, std::string> getMappings(const NginxConfig &config)
+{
+    std::map<std::string, std::string> handlerMap;
+    for(const auto& statement: config.statements_)
+    {
+        bool k1 = true;
+        bool k2 = true;
+        bool k3 = true;
+        for (const std::string& token : statement->tokens_)
+        {
+            if (!k1)
+            {
+                try
+                {
+                    // TODO Create handler.
+                    std::string s = "HelloWorldHandler";
+                    handlerMap.insert(std::pair<std::string,std::string>(token,s));
+                }
+                catch (...)
+                {
+                }
+            }
+            else if (!k2)
+            {
+                try
+                {
+                    // TODO Create handler.
+                    std::string s = "EchoHandler";
+                    handlerMap.insert(std::pair<std::string,std::string>(token,s));
+                }
+                catch (...)
+                {
+                }
+            }
+            else if (!k3)
+            {
+                try
+                {
+                    // TODO Create handler. Also store next token as path.
+                    std::string s = "StaticFileHandler";
+                    handlerMap.insert(std::pair<std::string,std::string>(token,s));
+                }
+                catch (...)
+                {
+                }
+            }
+            k1 = (token != "helloWorld");
+            k2 = (token != "echo");
+            k3 = (token != "static");
+        }
+    }
+    return handlerMap;
+}
