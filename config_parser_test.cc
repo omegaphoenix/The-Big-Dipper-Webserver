6a57947f46969baa054ee32d0e734c7e3a636b4a
Jonathan Joo <joo@caltech.edu>
2015-05-08 01:15:26 -0700

    added unit tests

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -38,9 +38,86 @@ protected:
         ParseString(config_string);
         return getPort(config_);
     }
+
+// Added 5/7/2015
+// Assignment 5 unit tests
+
+    int HandleHelloResponse(const std::string &config_string) {
+        HelloWorldHandler * a = new HelloWorldHandler();
+        std::string output;
+        size_t check_http200;
+        size_t check_content_type;
+        size_t check_hello;
+        size_t check_date;
+
+        output = a->handleRequests(config_string);
+        check_http200 = output.find("HTTP/1.0 200 OK\r\n"); //note- could not use protected var.
+        check_content_type = output.find("Content-Type: text/html;\r\n");  
+        check_hello = output.find("\r\n<html><body>Hello, world!</body></html>\r\n");
+        check_date = output.find("GMT");
+
+        if (check_http200 == std::string::npos ||
+            check_content_type == std::string::npos ||
+            check_hello == std::string::npos || 
+            check_date == std::string::npos) {
+            return 1;
+        }
+        return 0;     
+    }   
+
+    int HandleEchoResponse(const std::string &config_string) {
+        EchoHandler * a = new EchoHandler();
+        std::string output;
+        size_t check_http200;
+        size_t check_content_type;
+        size_t check_date;
+        size_t check_request;
+
+        output = a->handleRequests(config_string);
+        check_http200 = output.find("HTTP/1.0 200 OK\r\n"); //note- could not use protected var.
+        check_content_type = output.find("Content-Type: text/html;\r\n");  
+        check_date = output.find("GMT");
+        check_request = output.find(config_string);
+
+        if (check_http200 == std::string::npos ||
+            check_content_type == std::string::npos ||
+            check_date == std::string::npos ||
+            check_request == std::string::npos) {
+            return 1;
+        }
+        return 0;     
+    }  
+/*
+    int HandleStaticResponse(const std::string &config_string) {
+        StaticFileHandler * a = new StaticFileHandler();
+        std::string output;
+        size_t check_http200;
+        size_t check_content_type;
+        size_t check_content_type2;
+        size_t check_date;
+        size_t check_request;
+
+        output = a->handleRequests(config_string);
+        check_http200 = output.find("HTTP/1.0 200 OK\r\n"); //note- could not use protected var.
+        check_http404 = output.find("HTTP/1.0 404 Not Found\r\n"); // did not use
+        check_content_type = output.find("Content-Type: text/html;\r\n");  
+        check_content_type2 = output.find("Content-Type: image/jpeg;\r\n");
+        check_date = output.find("GMT");
+
+        // TODO: Need to do content checking
+
+        if (check_http200 == std::string::npos ||
+            check_content_type == std::string::npos ||
+            check_date == std::string::npos) {
+            return 1;
+        }
+        return 0;     
+    }  
+*/
     NginxConfigParser parser_;
     NginxConfig config_;
 };
+// ------------ //
 
 /*
  * Test written in class.
@@ -68,3 +145,25 @@ TEST_F(NginxStringConfigTest, NestedConfig) {
 TEST_F(NginxStringConfigTest, GetPortTest) {
     EXPECT_EQ(20, GetPort("port port 20;"));
 }
+
+
+// Writing more tests, 5/7/2015 Assignment 5
+
+TEST_F(NginxStringConfigTest, HandleHelloResponse) {
+    EXPECT_EQ(0, HandleHelloResponse("GET /hello HTTP/1.1"));
+}
+
+TEST_F(NginxStringConfigTest, HandleEchoResponse) {
+    EXPECT_EQ(0, HandleEchoResponse("GET /echo HTTP/1.1"));
+}
+
+/*
+TEST_F(NginxStringConfigTest, HandleStaticResponse) {
+    EXPECT_EQ(0, HandleStaticResponse("GET /static HTTP/1.1"));
+}
+
+*/
+
+
+
+
