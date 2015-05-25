cdd2974fad8bef451922b3cbe36a8920c9ec0b39
Jonathan Joo <joo@caltech.edu>
2015-05-25 16:53:15 -0700

    small changes to error_handler.cc

--- error_handler.cc
+++ error_handler.cc
@@ -6,7 +6,6 @@ void ErrorHandler::Configure(const NginxConfig& child_config_block) {
 
 std::string ErrorHandler::HandleRequest(const HTTPRequest& req) {
     std::string date = makeDaytimeString();
-    return http404 + date + "\r\n"
-            "Error 404: Not Found.";
+    return http404 + date + "\r\n" + "Error 404: Not Found.";
    
 }
